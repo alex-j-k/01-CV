85e7092b5b9fffcd8892b78e2cba4344562422ce
alex-j-k <63852006+alex-j-k@users.noreply.github.com>
2020-11-19 22:50:14 +1000

    add weather

--- CV.js
+++ CV.js
@@ -1,10 +1,80 @@
-const images = document.querySelectorAll('.upscale');
-console.log(images);
 
-function toggleUpsizeClass() {
-    this.classList.toggle('upsize')
+
+// WEATHER API
+
+const APIkey = 'tFlfX5ZFpnWErhD84pVb2Vhrb4J66IW4';
+const BNEkey = 24741;
+
+// GET WEATHER INFO - CURRENT CONDITIONS
+
+
+
+const getWeather = async (cityKey) => {
+
+const URLbase = 'http://dataservice.accuweather.com/currentconditions/v1/';
+const queryParams = `${cityKey}?apikey=${APIkey}`;
+
+const response = await fetch(URLbase + queryParams);
+const data = await response.json();
+
+return data[0];
+
 };
 
-images.forEach((image) => {image.addEventListener('mouseover', toggleUpsizeClass)});
-images.forEach((image) => {image.addEventListener('mouseleave', toggleUpsizeClass)});
+
+//GET CITY INFO
+const getCity = async (city) => {
+
+    const URLbase = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const queryParams = `?apikey=${APIkey}&q=${city}`;
+
+    const response = await fetch(URLbase + queryParams);
+    const data = await response.json();
+
+    return (data[0])
+};
+
+getCity('brisbane')
+    .then(data => {
+        return getWeather(data.Key);
+    }).then(data => console.log(data.WeatherText))
+    .catch(error => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+// FETCH
+
+
+// fetch(endpoint)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+
+
+
+
+
+
+
+
+
+
+
+// const images = document.querySelectorAll('.upscale');
+// console.log(images);
+
+// function toggleUpsizeClass() {
+//     this.classList.toggle('upsize')
+// };
+
+// images.forEach((image) => {image.addEventListener('mouseover', toggleUpsizeClass)});
+// images.forEach((image) => {image.addEventListener('mouseleave', toggleUpsizeClass)});
 
